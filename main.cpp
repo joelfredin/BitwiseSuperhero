b956fa7e8bc024bef92f0dcb03ab6fc4f0cef269
Joel Fredin <joel.l.fredin@gmail.com>
2023-05-27 23:26:24 +0200

    adding functionality of revoking powers and checking if hero has certain powers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstdint>
 #include<functional>
+#include<type_traits>
 
 /*
 POSSIBLE SUPERPOWERS
@@ -16,6 +17,11 @@ enum class Powers : std::uint8_t
     TELEPORTATION = 1 << 5,
     TRANSFORMATION = 1 << 6,
     TELEKINESIS = 1 << 7
+};
+
+struct Superhero
+{
+    Powers powers{Powers::HUMAN};
 };
 
 template<typename EnumType>
@@ -40,15 +46,22 @@ std::enable_if_t<has_bitwise_operator<EnumType>, EnumType> bitwise_operator(Enum
     return static_cast<EnumType>(op(static_cast<Underlying>(lhs),static_cast<Underlying>(rhs)));
 }
 
-
-struct Superhero
+template<typename EnumType, typename BitwiseOp>
+std::enable_if_t<has_bitwise_operator<EnumType>, EnumType> bitwise_complement(EnumType lhs, BitwiseOp op)
 {
-    Powers powers{Powers::HUMAN};
-};
+    using Underlying = std::underlying_type_t<EnumType>;
+    return static_cast<EnumType>(op(static_cast<Underlying>(lhs)));
+}
+
 
 void enablePower(Superhero& hero, Powers power)
 {
     hero.powers = bitwise_operator(hero.powers, power, std::bit_or<>{});
+}
+
+void revokePower(Superhero& hero, Powers power)
+{
+    hero.powers = bitwise_operator(hero.powers, bitwise_complement(power, std::bit_not<>{}), std::bit_and<>{});
 }
 
 
@@ -62,33 +75,31 @@ void printBinary(Powers superpower)
     std::cout << std::endl;
 }
 
-/*
-bool checkSuperpower(Superhero hero, Powers superpower_to_check)
+bool hasPower(Superhero hero, Powers power)
 {
-    if((hero.powers & superpower_to_check) != 0)
+    if(static_cast<int>(bitwise_operator(hero.powers,power, std::bit_and<>{})))
     {
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
-*/
 
 int main()
 {
     Superhero hero1;
 
-    //Powers result_or = bitwise_operator(Powers::FLIGHT, Powers::TELEKINESIS, std::bit_or<>{});
     printBinary(hero1.powers);
 
     enablePower(hero1, Powers::FLIGHT);
     enablePower(hero1, Powers::TELEKINESIS);
 
-    //hero1.powers = result_or;
-
-    //std::cout << static_cast<std::underlying_type_t<std::uint8_t>>(result_or) << std::endl;
-
-    //std::cout << static_cast<int>(result_or) << std::endl;
     printBinary(hero1.powers);
+
+    revokePower(hero1, Powers::TELEKINESIS);
+
+    printBinary(hero1.powers);
+
+    std::cout << hasPower(hero1, Powers::FLIGHT) << std::endl;
 
     return 0;
 }
